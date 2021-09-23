36ab6c360aa7a6de80f60f5756d93aa45a7e5fff
Adamor Henner <adacandido9@gmail.com>
2021-09-23 11:07:57 -0300

    Criando repository com spring data

--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,5 +1,6 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +8,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
+
 /**
  *
  * A sample greetings controller to return greeting text
  */
 @RestController
 public class GreetingsController {
-    /**
+    
+	@Autowired /*IC/CD ou CDI - Injeção de dependencia*/
+	private UsuarioRepository usuarioRepository;
+	
+	
+	
+	
+	/**
      *
      * @param name the name to greet
      * @return greeting text
@@ -28,6 +39,12 @@ public class GreetingsController {
     @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String retornaOlaMundo(@PathVariable String nome) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(nome);
+    	
+    	usuarioRepository.save(usuario);
+    	
     	return "Ola mundo " + nome;
     }
     
